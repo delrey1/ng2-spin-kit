f085cf7ff6c04ef1507a1ce073955e3a55a0dcc2
jorge <j@j>
2023-06-19 20:02:49 +0100

    Properly removing previously deleted component

--- projects/ng2-spin-kit2/src/public-api.ts
+++ projects/ng2-spin-kit2/src/public-api.ts
@@ -3,6 +3,5 @@
  */
 
 export * from './lib/ng2-spin-kit2.service';
-export * from './lib/ng2-spin-kit2.component';
 export * from './lib/ng2-spin-kit2.module';
 export * from './lib/spinners/wave.component'
